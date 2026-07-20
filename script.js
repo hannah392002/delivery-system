e52b1c27a3a86e340fe5542c3a6454162b1b86dc
hannah392002 <132369364+hannah392002@users.noreply.github.com>
2026-07-20 13:58:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,39 @@ function deleteBook(btn){
 btn.parentElement.parentElement.remove();
 
 }
+function saveOrder(){
+
+let name=document.getElementById("name").value;
+
+let phone=document.getElementById("phone1").value;
+
+let order=document.getElementById("orderNo").value;
+
+let books=document.querySelectorAll("#booksTable tbody tr").length;
+
+let table=document.querySelector("#ordersTable tbody");
+
+let row=document.createElement("tr");
+
+row.innerHTML=`
+
+<td>${order}</td>
+
+<td>${name}</td>
+
+<td>${phone}</td>
+
+<td>${books}</td>
+
+<td><button onclick="loadOrder(this)">فتح</button></td>
+
+`;
+
+table.appendChild(row);
+
+clearForm();
+
+}
 
 function saveOrder(){
 
@@ -39,5 +72,28 @@ alert("سيتم إضافة الحفظ الكامل في الجزء القادم"
 window.onload=function(){
 
 addBook();
+
+}
+function clearForm(){
+
+document.getElementById("orderNo").value="";
+
+document.getElementById("name").value="";
+
+document.getElementById("phone1").value="";
+
+document.getElementById("phone2").value="";
+
+document.getElementById("address").value="";
+
+document.querySelector("#booksTable tbody").innerHTML="";
+
+addBook();
+
+}
+
+function loadOrder(btn){
+
+alert("سيتم فتح الطلب في الخطوة القادمة");
 
 }
